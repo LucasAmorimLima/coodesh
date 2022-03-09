f2557120cec4c84d49c2de59d62e0f77e85bf0f8
Lucas Amorim Lima <lucasasmorimlima17@gmail.com>
2022-03-09 09:05:39 -0300

    some fixes in validator

--- src/controllers/validator/index.ts
+++ src/controllers/validator/index.ts
@@ -1,4 +1,3 @@
-import { Articles } from "../../domain/articles";
 import { CreateArticlesDto } from "../../use-cases/dto/createArticlesDto";
 export interface Validator {
     createValidate(data: CreateArticlesDto): boolean
